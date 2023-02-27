dec43d51dec17071d0b33984519e39aa668ac15c
Dotnet9(dotnet9.com) <1012434131@qq.com>
2023-02-27 22:41:16 +0800

    添加动态加载组件代码；

---
+++ src/LoadXamlSample/WFHost/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Activities;
+using System.Activities.XamlIntegration;
+using System.IO;
+using System.Text;
+using System.Windows;
+
+namespace WFHost
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void RunButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            var utf8 = new UTF8Encoding();
+            var bs = utf8.GetBytes(this.Run.Text);
+            var memoryStream = new MemoryStream(bs);
+            var activity = ActivityXamlServices.Load(memoryStream);
+            var instance = new WorkflowApplication(activity);
+            instance.Run();
+        }
+    }
+}
