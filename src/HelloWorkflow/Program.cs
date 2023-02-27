f45305af64c62a20607bbc5e38656a6e276f77bc
Dotnet9(dotnet9.com) <1012434131@qq.com>
2023-02-27 16:02:34 +0800

    添加代码方式创建流程；

--- src/HelloWorkflow/Program.cs
+++ src/HelloWorkflow/Program.cs
@@ -1,13 +1,24 @@
-﻿using System;
-using System.Activities;
+﻿using System.Activities;
 using System.Activities.Statements;
-using System.Linq;
 
-namespace HelloWorkflow {
-    internal class Program {
-        static void Main(string[] args) {
-            Activity workflow1 = new Workflow1();
-            WorkflowInvoker.Invoke(workflow1);
+namespace HelloWorkflow
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //WorkflowInvoker.Invoke(new Workflow1());  // 1、设计器设计流程
+
+            WorkflowInvoker.Invoke(CreateWorkflowFromCode());   // 2、代码方式生成流程
+        }
+
+        static Activity CreateWorkflowFromCode()
+        {
+            WriteLine writeLine=new WriteLine(){Text = "测试代码创建活动"};
+            Sequence sequence = new Sequence();
+            sequence.Activities.Add(writeLine);
+
+            return sequence;
         }
     }
 }
