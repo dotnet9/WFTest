04c558829fad200e35453eda53a393434339272b
Dotnet9(dotnet9.com) <1012434131@qq.com>
2023-02-28 16:36:31 +0800

    添加并行、循环等活动控制；

---
+++ src/ControlActivitySample2/ControlActivitySample2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Activities;
+
+namespace ControlActivitySample2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //WorkflowInvoker.Invoke(new ParallelWorkflow());
+
+            //WorkflowInvoker.Invoke(new ParallelCompletionConditionWorkflow());
+
+            // WorkflowInvoker.Invoke(new ParallelForEachWorkflow());
+
+            WorkflowInvoker.Invoke(new PickWorkflow());
+            System.Console.Read();
+        }
+    }
+}
